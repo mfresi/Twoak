45cb50ac81004546b58f70d5bb56687577801e5c
eflaquet <edouard.flaquet@gmail.com>
2021-02-09 10:38:13 +0100

    Testcommit

--- Class/ClassUser.php
+++ Class/ClassUser.php
@@ -22,7 +22,7 @@ class user
     public function connectbdd() 
     {
         try {
-            $bdd = new PDO('mysql:host=localhost; dbname=twoak; charset=utf8', 'root', 'root');
+            $bdd = new PDO('mysql:host=localhost; dbname=Twoak; charset=utf8', 'root', 'root');
         } catch (Exception $erreur) {
             echo 'Erreur : ' . $erreur->getMessage();
         }
