747bb2efecba3fa861db9543eb4f9b727b0dfe66
eflaquet <edouard.flaquet@gmail.com>
2021-02-12 09:26:13 +0100

    Update status

--- deconnexion.php
+++ deconnexion.php
@@ -1,4 +1,8 @@
-<?php
+<?php include "database.php";
+global $bdd;
+
+$deconnect = $bdd->prepare("UPDATE User SET user_status = 0 WHERE ID_User = ". $_SESSION['id']);
+            $deconnect->execute();
     session_start();
     session_unset();
     session_destroy(); 
