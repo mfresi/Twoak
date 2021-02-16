7fc9fffa0956c5d83a5948e1ce1855e3e2a7a11d
Colbert1 <gcolbert@la-providence.net>
2021-02-16 11:09:26 +0100

    test non concluant follow

--- follow.php
+++ follow.php
@@ -1,19 +1,15 @@
 <?php
 session_start();
 $user_id = $_SESSION['user_id'];
-include('database.php');
-?>
-<?php
-if ($_GET['id']  && $_GET['user_login']) {
-    if ($_GET['id'] != $user_id) {
-        $follow_userid = $_GET['id'];
-        $follow_username = $_GET['user_login'];
-        $query = $bdd->query("SELECT ID_User FROM Follow WHERE Fol_ID_Owner='$user_id' AND Fol_ID_Follower='$follow_userid'");
-        if (!(($query) >= 1)) {
-            $bdd->query("INSERT INTO Follow (Fol_ID_Owner, Fol_ID_Follower)  VALUES ('$user_id', '$follow_userid')");
-            $bdd->query("UPDATE User SET Follow = Follow + 1 WHERE ID_User='$user_id'");
-            $bdd->query("UPDATE User SET user_follower = follower + 1 WHERE ID_User='$follow_userid'");
-        }
+
+if (isset($_GET['follow'])) {
+    $follow = $bdd->prepare('INSERT INTO `Follow`(`Fol_ID_Owner`, `Fol_ID_Follower`) VALUES (:Fol_ID_Owner,:Fol_ID_Follower)');
+    $follow->execute();
+    $this->$bdd->bind('Fol_ID_Follower', $follower_id);
+    $this->$bdd->bind('Fol_ID_Owner', $following_id);
+    if ($this->$bdd->execute()) {
+        return true;
+    } else {
+        return false;
     }
 }
-?>
