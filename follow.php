b4f14e59cd72501fbe4f07f0c5bc6ab04c2c425b
Colbert1 <gcolbert@la-providence.net>
2021-02-16 09:25:59 +0100

    commit change

--- follow.php
+++ follow.php
@@ -10,11 +10,10 @@ if ($_GET['id']  && $_GET['user_login']) {
         $follow_username = $_GET['user_login'];
         $query = $bdd->query("SELECT ID_User FROM Follow WHERE Fol_ID_Owner='$user_id' AND Fol_ID_Follower='$follow_userid'");
         if (!(($query) >= 1)) {
-            $bdd->query("INSERT INTO Follow(Fol_ID_Owner, Fol_ID_Follower)  VALUES ('$user_id', '$follow_userid')");
+            $bdd->query("INSERT INTO Follow (Fol_ID_Owner, Fol_ID_Follower)  VALUES ('$user_id', '$follow_userid')");
             $bdd->query("UPDATE User SET Follow = Follow + 1 WHERE ID_User='$user_id'");
             $bdd->query("UPDATE User SET user_follower = follower + 1 WHERE ID_User='$follow_userid'");
         }
-        header("Location: ./" . $follow_username);
     }
 }
 ?>
