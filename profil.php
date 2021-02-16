31508d7ddf398362801333c726723a84b85aa954
Colbert1 <gcolbert@la-providence.net>
2021-02-16 11:10:51 +0100

    button et page un follow

--- profil.php
+++ profil.php
@@ -106,6 +106,10 @@ global $bdd;
 					<form action="" method="GET">
 						<button title="Follow" data-ripple="" id="butFollow" name="follow">Follow</button>
 					</form>
+					<!-- Bouton unfollow -->
+					<form action="" method="GET">
+						<button title="Unfollow" data-ripple="" id="butUnfollow" name="unfollow">Unfollow</button>
+					</form>
 				</div>
 				<form method="POST" class="edit-phto" enctype="multipart/form-data">
 					<i class="fa fa-camera-retro"></i>
